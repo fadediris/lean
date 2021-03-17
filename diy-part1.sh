039c54a9f1878b74c21f80b4e4e4fe76ad9a5987
fadediris <33453792+fadediris@users.noreply.github.com>
2021-03-17 21:05:15 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,7 +11,12 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+git clone https://github.com/tty228/luci-app-serverchan.git package/DIY/luci-app-serverchan
+git clone https://github.com/lisaac/luci-lib-docker.git package/DIY/luci-lib-docker
+git clone https://github.com/lisaac/luci-app-dockerman.git package/DIY/luci-app-dockerman
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/DIY/luci-app-adguardhome
