d77224d885e59dcbd2c26ea2c7f5e3a331d273cf
fadediris <33453792+fadediris@users.noreply.github.com>
2021-06-11 17:36:08 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,9 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+#Docker
+if [ -e feeds/packages/utils/docker-ce ];then
+	sed -i '/dockerd/d' package/luci-app-dockerman/Makefile	
+  sed -i 's/+docker/+docker-ce/g' package/luci-app-dockerman/Makefile
+fi
